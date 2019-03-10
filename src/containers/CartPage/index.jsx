300b3f456be6e88fe5f5d78a6f039c80bfc74a41
ibrahimalausa <ibrahim.alausa@andela.com>
2019-03-10 17:25:37 +0100

    Add rendered component test for cart page

--- src/containers/CartPage/index.jsx
+++ src/containers/CartPage/index.jsx
@@ -4,10 +4,6 @@ import ShoppingCart from '../../components/ShoppingCart';
 
 export default class CartPage extends Component {
   render() {
-    //  const currentItems = JSON.parse(localStorage.getItem('myCart'));
-    //  if (!currentItems) {
-    //    localStorage.setItem('myCart', JSON.stringify([]));
-    //  }
     return <Navbar displayPage={<ShoppingCart />} />;
   }
 }
